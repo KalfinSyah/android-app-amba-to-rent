3da78eea0762b83b7f17ef030f2e5acd5018e225
Irsyad Fadhil <irsyadfadhil123@gmail.com>
2025-12-16 18:10:17 +0700

    fix dashboard admin again

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
 use App\Http\Controllers\Web\CarController;
+use App\Http\Controllers\Web\DashboardController;
 use App\Http\Controllers\Web\OrderController;
 use App\Http\Controllers\Web\PenaltyController;
 use App\Http\Controllers\Web\ProfileController;
 use App\Http\Controllers\Web\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', DashboardController::class)->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
